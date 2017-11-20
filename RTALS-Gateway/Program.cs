9fe2e3231b6a69707cda66d9f48c40c3fbb479e5
Enrico Ventola <enricoventola@sytner.co.uk>
2017-11-20 00:22:56 +0000

    Add project files.

---
+++ RTALS-Gateway/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RTALS_Gateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
